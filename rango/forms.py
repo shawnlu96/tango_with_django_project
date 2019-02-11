e077001c552c581a72b0d90748d7f47aeef84a09
Xiaoyu Lu <2387688l@student.gla.ac.uk>
2019-02-11 21:29:38 +0000

    first overall debugged version

--- rango/forms.py
+++ rango/forms.py
@@ -21,8 +21,8 @@ class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
                             help_text="Please enter the title of the page.")
 
-    url = forms.CharField(max_length=200,
-                          help_text="Please enter the URL of the page.")
+    url = forms.URLField(max_length=200,
+                         help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     def clean(self):
