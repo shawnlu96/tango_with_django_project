ac4a5f8cc88f3961393e063a4a31f8cad4ac977c
Xiaoyu Lu <2387688l@student.gla.ac.uk>
2019-02-11 22:20:56 +0000

    overall debugged version 2

--- rango/forms.py
+++ rango/forms.py
@@ -43,9 +43,9 @@ class PageForm(forms.ModelForm):
         # Some fields may allow NULL values, so we may not want to include them.
         # Here, we are hiding the foreign key.
         # we can either exclude the category field from the form,
-        exclude = ('category',)
+        # exclude = ('category',)
         # or specify the fields to include (i.e. not include the category field)
-        # fields = ('title', 'url', 'views')
+        fields = ('title', 'url', 'views')
 
 
 class UserForm(forms.ModelForm):
