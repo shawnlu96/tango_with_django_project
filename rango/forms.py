9a4539a26abe26fc4355cf85d96a33b9b9e505bf
Xiaoyu Lu <2387688l@student.gla.ac.uk>
2019-02-11 04:16:49 +0000

    chapter 7 finished

--- rango/forms.py
+++ rango/forms.py
@@ -15,22 +15,33 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ('name',)
 
-    class PageForm(forms.ModelForm):
-        title = forms.CharField(max_length=128,
-                                help_text="Please enter the title of the page.")
 
-        url = forms.URLField(max_length=200,
-                             help_text="Please enter the URL of the page.")
-        views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128,
+                            help_text="Please enter the title of the page.")
 
-        class Meta:
-            # Provide an association between the ModelForm and a model
-            model = Page
-            # What fields do we want to include in our form?
-            # This way we don't need every field in the model present.
-            # Some fields may allow NULL values, so we may not want to include them.
-            # Here, we are hiding the foreign key.
-            # we can either exclude the category field from the form,
-            exclude = ('category',)
-            # or specify the fields to include (i.e. not include the category field)
-            # fields = ('title', 'url', 'views')
+    url = forms.CharField(max_length=200,
+                          help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+            return cleaned_data
+
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Page
+        # What fields do we want to include in our form?
+        # This way we don't need every field in the model present.
+        # Some fields may allow NULL values, so we may not want to include them.
+        # Here, we are hiding the foreign key.
+        # we can either exclude the category field from the form,
+        exclude = ('category',)
+        # or specify the fields to include (i.e. not include the category field)
+        # fields = ('title', 'url', 'views')
