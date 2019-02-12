2582460453f39947c4fd41bc7fc110667431953b
Xiaoyu Lu <2387688l@student.gla.ac.uk>
2019-02-12 17:15:25 +0000

    bug fixed

--- rango/models.py
+++ rango/models.py
@@ -26,7 +26,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    slug = models.SlugField(unique=true, default='null')
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
